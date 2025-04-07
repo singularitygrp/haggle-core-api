3e325dee7378dde2ce057ed53940d8bbcd695768
Tomas Taylor <totaylor19@gmail.com>
2025-04-07 09:45:57 -0700

    Update memory.ts

--- src/ai/agents/memory.ts
+++ src/ai/agents/memory.ts
@@ -1,5 +1,5 @@
 import { InMemoryStore } from '@langchain/langgraph';
 import { MemorySaver } from '@langchain/langgraph-checkpoint';
 
-export const memorySaver = new MemorySaver();
-export const inMemoryStore = new InMemoryStore();
+export const store = new InMemoryStore();
+export const checkpointer = new MemorySaver();
