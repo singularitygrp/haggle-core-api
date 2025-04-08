058fdffc390867ff250f05e4f96dc3d3f4bb8f0a
Tomas Taylor <totaylor19@gmail.com>
2025-04-07 18:07:30 -0700

    Update supervisor.ts

--- src/ai/agents/supervisor.ts
+++ src/ai/agents/supervisor.ts
@@ -9,53 +9,57 @@ import { initChatModel } from 'langchain/chat_models/universal';
 
 import { PriceFinder } from './price-finder';
 
-const supervisor = async (state: typeof MessagesAnnotation.State) => {
-  const model = await initChatModel('gpt-4o-mini', {
-    temperature: 0,
-  });
-
-  const response = await model
-    .withStructuredOutput(
-      z.object({
-        next_agent: z.enum(['agent1', 'agent2', '__end__']),
-      }),
-    )
-    .invoke(state.messages);
-  return new Command({
-    goto: response.next_agent,
-  });
-};
-
-const agent1 = async (state: typeof MessagesAnnotation.State) => {
-  const model = await initChatModel('gpt-4o-mini', {
-    temperature: 0,
-  });
-
-  const response = await model.invoke(state.messages);
-  return new Command({
-    goto: 'supervisor',
-    update: {
-      messages: [response],
-    },
-  });
-};
-
-const agent2 = async (state: typeof MessagesAnnotation.State) => {
-  const model = await initChatModel('gpt-4o-mini', {
-    temperature: 0,
-  });
-
-  const response = await model.invoke(state.messages);
-  return new Command({
-    goto: 'supervisor',
-    update: {
-      messages: [response],
-    },
-  });
-};
-
 export class Supervisor {
+  private async createAgentOne(state: typeof MessagesAnnotation.State) {
+    const model = await initChatModel('gpt-4o-mini', {
+      temperature: 0,
+    });
+
+    const response = await model.invoke(state.messages);
+    return new Command({
+      goto: 'supervisor',
+      update: {
+        messages: [response],
+      },
+    });
+  }
+
+  private async createAgentTwo(state: typeof MessagesAnnotation.State) {
+    const model = await initChatModel('gpt-4o-mini', {
+      temperature: 0,
+    });
+
+    const response = await model.invoke(state.messages);
+    return new Command({
+      goto: 'supervisor',
+      update: {
+        messages: [response],
+      },
+    });
+  }
+
+  private async createSupervisor(state: typeof MessagesAnnotation.State) {
+    const model = await initChatModel('gpt-4o-mini', {
+      temperature: 0,
+    });
+
+    const response = await model
+      .withStructuredOutput(
+        z.object({
+          next_agent: z.enum(['agent1', 'agent2', '__end__']),
+        }),
+      )
+      .invoke(state.messages);
+    return new Command({
+      goto: response.next_agent,
+    });
+  }
+
   getWorkflow() {
+    const agent1 = this.createAgentOne;
+    const agent2 = this.createAgentTwo;
+    const supervisor = this.createSupervisor;
+
     return new StateGraph(MessagesAnnotation)
       .addNode('supervisor', supervisor, {
         ends: ['agent1', 'agent2', '__end__'],
